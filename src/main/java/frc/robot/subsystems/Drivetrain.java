4e8283125abf58a06060a7dea33d064238693728
FizzyApple12 <davideroeder@gmail.com>
2022-02-01 17:59:22 -0500

    renamed motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.music.Orchestra;
 
 import frc.robot.RobotMap;
@@ -11,36 +11,36 @@ import frc.robot.utilities.di.DiInterfaces.IDisposable;
 import frc.robot.utilities.di.DiInterfaces.IInitializable;
 
 public class Drivetrain extends DiSubsystem implements IInitializable, IDisposable {
-    private TalonFX leftDom = new TalonFX(RobotMap.Drivetrain.LEFT_DOM_CHANNEL);
-    private TalonFX leftSub = new TalonFX(RobotMap.Drivetrain.LEFT_SUB_CHANNEL);
+    private WPI_TalonFX leftLeader = new TalonFX_WPI(RobotMap.Drivetrain.LEFT_LEADER_CHANNEL);
+    private WPI_TalonFX leftFollower = new TalonFX_WPI(RobotMap.Drivetrain.LEFT_FOLLOWER_CHANNEL);
 
-    private TalonFX rightDom = new TalonFX(RobotMap.Drivetrain.RIGHT_DOM_CHANNEL);
-    private TalonFX rightSub = new TalonFX(RobotMap.Drivetrain.RIGHT_SUB_CHANNEL);
+    private WPI_TalonFX rightLeader = new TalonFX_WPI(RobotMap.Drivetrain.RIGHT_LEADER_CHANNEL);
+    private WPI_TalonFX rightFollower = new TalonFX_WPI(RobotMap.Drivetrain.RIGHT_FOLLOWER_CHANNEL);
 
     private Orchestra orchestra = new Orchestra();
 
     public void onInitialize() {
-        this.orchestra.addInstrument(this.leftDom);
-        this.orchestra.addInstrument(this.leftSub);
+        this.orchestra.addInstrument(this.leftLeader);
+        this.orchestra.addInstrument(this.leftFollower);
         
-        this.orchestra.addInstrument(this.rightDom);
-        this.orchestra.addInstrument(this.rightSub);
+        this.orchestra.addInstrument(this.rightLeader);
+        this.orchestra.addInstrument(this.rightFollower);
 
-        this.leftDom.setNeutralMode(NeutralMode.Brake);
-        this.leftSub.setNeutralMode(NeutralMode.Brake);
-        this.rightDom.setNeutralMode(NeutralMode.Brake);
-        this.rightSub.setNeutralMode(NeutralMode.Brake);
+        this.leftLeader.setNeutralMode(NeutralMode.Brake);
+        this.leftFollower.setNeutralMode(NeutralMode.Brake);
+        this.rightLeader.setNeutralMode(NeutralMode.Brake);
+        this.rightFollower.setNeutralMode(NeutralMode.Brake);
 
-        this.rightDom.setInverted(true);
-        this.rightSub.setInverted(true);
+        this.rightLeader.setInverted(true);
+        this.rightFollower.setInverted(true);
 
-        this.leftSub.follow(this.leftDom);
-        this.rightSub.follow(this.rightDom);
+        this.leftFollower.follow(this.leftLeader);
+        this.rightFollower.follow(this.rightLeader);
     }
 
     public void run(double leftPower, double rightPower) {
-        this.leftDom.set(ControlMode.PercentOutput, leftPower);
-        this.rightDom.set(ControlMode.PercentOutput, rightPower);
+        this.leftLeader.set(ControlMode.PercentOutput, leftPower);
+        this.rightLeader.set(ControlMode.PercentOutput, rightPower);
     }
 
     public void singTheTheme() {
