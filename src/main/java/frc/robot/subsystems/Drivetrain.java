91cedc65af3e42c06b7f50844559f6520d332354
SB510 <sean.borneman@gmail.com>
2022-02-09 16:03:08 -0500

    implemented senable of drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,12 +5,15 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.music.Orchestra;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.RobotMap;
 import frc.robot.utilities.DiSubsystem;
 import frc.robot.utilities.di.DiInterfaces.IDisposable;
 import frc.robot.utilities.di.DiInterfaces.IInitializable;
 
-public class Drivetrain extends DiSubsystem implements IInitializable, IDisposable {
+public class Drivetrain extends DiSubsystem implements IInitializable, IDisposable, Sendable {
     private WPI_TalonFX leftLeader = new WPI_TalonFX(RobotMap.Drivetrain.LEFT_LEADER_CHANNEL);
     private WPI_TalonFX leftFollower = new WPI_TalonFX(RobotMap.Drivetrain.LEFT_FOLLOWER_CHANNEL);
 
@@ -41,6 +44,15 @@ public class Drivetrain extends DiSubsystem implements IInitializable, IDisposab
     public void run(double leftPower, double rightPower) {
         this.leftLeader.set(ControlMode.PercentOutput, leftPower);
         this.rightLeader.set(ControlMode.PercentOutput, rightPower);
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+      builder.setSmartDashboardType("DifferentialDrive");
+      builder.setActuator(true);
+      builder.setSafeState(() -> this.run(0, 0));
+      builder.addDoubleProperty("Left Motor Speed", this.leftLeader::get, this.leftLeader::set);
+      builder.addDoubleProperty("Right Motor Speed", this.rightLeader::get, this.rightLeader::set);
     }
 
     public void singTheTheme() {
